7edee5d7c0e1a6c46bcf4f43989112c48e6ce2a8
Gabriel Couto <106809403+GabrielRicardo1@users.noreply.github.com>
2026-04-15 21:52:46 -0300

    Update Labirinto.c

--- TrabalhoN1Algoritmos2/Labirinto.c
+++ TrabalhoN1Algoritmos2/Labirinto.c
@@ -188,28 +188,28 @@ void telaDerrota()
     printf(RED);
     printf("\n\n");
     printf(
-    "     .... NO! ...                  ... MNO! ...\n");
-    "   ..... MNO!! ...................... MNNOO! ...\n");
-    " ..... MMNO! ......................... MNNOO!! .\n");
-    ".... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .\n");
-    " ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....\n");
-    "    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...\n");
-    "   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....\n");
-    "   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...\n");
-    "    ....... MMMMM..    OPPMMP    .,OMI! ....\n");
-    "     ...... MMMM::   o.,OPMP,.o   ::I!! ...\n");
-    "         .... NNM:::.,,OOPM!P,.::::!! ....\n");
-    "          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....\n");
-    "         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....\n");
-    "           .. MMMMMNNOOMMNNIIIPPPOO!! ......\n");
-    "          ...... MMMONNMMNNNIIIOO!..........\n");
-    "       ....... MN MOMMMNNNIIIIIO! OO ..........\n");
-    "    ......... MNO! IiiiiiiiiiiiI OOOO ...........\n");
-    "  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........\n");
-    "   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........\n");
-    "   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........\n");
-    "      ...... OO! ................. ON! .......\n");
-    "         ................................\n");
+    "     .... NO! ...                  ... MNO! ...\n"
+    "   ..... MNO!! ...................... MNNOO! ...\n"
+    " ..... MMNO! ......................... MNNOO!! .\n"
+    ".... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .\n"
+    " ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....\n"
+    "    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...\n"
+    "   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....\n"
+    "   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...\n"
+    "    ....... MMMMM..    OPPMMP    .,OMI! ....\n"
+    "     ...... MMMM::   o.,OPMP,.o   ::I!! ...\n"
+    "         .... NNM:::.,,OOPM!P,.::::!! ....\n"
+    "          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....\n"
+    "         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....\n"
+    "           .. MMMMMNNOOMMNNIIIPPPOO!! ......\n"
+    "          ...... MMMONNMMNNNIIIOO!..........\n"
+    "       ....... MN MOMMMNNNIIIIIO! OO ..........\n"
+    "    ......... MNO! IiiiiiiiiiiiI OOOO ...........\n"
+    "  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........\n"
+    "   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........\n"
+    "   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........\n"
+    "      ...... OO! ................. ON! .......\n"
+    "         ................................\n"
     );
     printf("\n");
     printf("########################################\n");
